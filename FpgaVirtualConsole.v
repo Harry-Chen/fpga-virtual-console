1b039760ef0911e661b05a43304de731cd5a2e57
Harry Chen <i@harrychen.xyz>
2018-05-03 16:28:20 +0800

    Add uart module connection
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- FpgaVirtualConsole.v
+++ FpgaVirtualConsole.v
@@ -1,8 +1,58 @@
 module FpgaVirtualConsole(
-input hello,
-output world
+    input               clk,
+    input               rst,
+    // uart transceiver
+    input               uartRx,
+    output  reg         uartTx,
+    // vga output
+    output  reg         vgaHSync,
+    output  reg         vgaVSync,
+    output  reg [2:0]   vgaRed,
+    output  reg [2:0]   vgaGreen,
+    output  reg [2:0]   vgaBlue,
+    // debug output
+    output  reg [47:0]  segments,
+    output  reg [15:0]  leds
 );
 
-assign world = hello;
+reg         uartReady;
+reg [7:0]   uartDataReceived;
+reg         uartStartSend;
+reg [7:0]   uartDataToSend;
+reg         uartBusy;
+
+wire        uartTxSignal;
+wire        uartBusySignal;
+wire        uartReadySignal;
+wire [7:0]  uartDataSignal;
+
+always @(posedge clk)
+begin
+    uartTx <= uartTxSignal;
+    uartBusy <= uartBusySignal;
+    uartReady <= uartReadySignal;
+    uartDataReceived <= uartDataSignal;
+end
+
+async_transmitter #(
+    .ClkFrequency(1000000),
+    .Baud(115200)
+) uartTransmitter(
+    .clk(clk), // input
+    .TxD_start(uartStartSend), // input
+    .TxD_data(uartDataToSend), // input
+    .TxD(uartTxSignal), // output
+    .TxD_busy(uartBusySignal) // output
+);
+
+async_receiver #(
+    .ClkFrequency(1000000),
+    .Baud(115200)
+) uartReceiver(
+	.clk(clk), // input
+	.RxD(uartRx), // input
+	.RxD_data_ready(uartReadySignal), // output
+	.RxD_data(uartDataSignal), // output
+);
 
 endmodule
