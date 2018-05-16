fe024abd40fc834b2a15b72912ec475a78158b6c
Harry Chen <i@harrychen.xyz>
2018-05-16 15:39:48 +0800

    Add TextBuffer memory
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- FontShapeRenderer.sv
+++ FontShapeRenderer.sv
@@ -1,11 +1,16 @@
 module FontShapeRenderer (
-    input clk,
-    input rst,
-    input [7:0] char,
-    input []
-
+    input   clk,
+    input   rst,
+    input   [7:0] char,
+    input   [COLOR_NUMBERS_BITS - 1:0] foregroudColor;
+    input   [COLOR_NUMBERS_BITS - 1:0] backgroundColor;
+    output  [HEIGHT_PER_CHARACTER * WIDTH_PER_CHARACTER - 1:0] fontColorR;
+    output  [HEIGHT_PER_CHARACTER * WIDTH_PER_CHARACTER - 1:0] fontColorG;
+    output  [HEIGHT_PER_CHARACTER * WIDTH_PER_CHARACTER - 1:0] fontColorB;
 );
 
-parameter COLOR_NUMBERS = 4;
+parameter COLOR_NUMBERS_BITS = 4
+parameter HEIGHT_PER_CHARACTER = 20;
+parameter WIDTH_PER_CHARACTER = 8;
 
-endmodule // FontShapeRender
+endmodule // FontShapeRender;
--- TextRenderer.sv
+++ TextRenderer.sv
@@ -2,10 +2,17 @@ module TextRenderer(
     input clk,
     input rst,
     input [CONSOLE_LINES * CONSOLE_COLUMNS * 8 - 1:0] text,
-    input [CONSOLE_LINES * CONSOLE_COLUMNS * COLOR_NUMBERS:0] color,
+    input [CONSOLE_LINES * CONSOLE_COLUMNS * COLOR_NUMBERS_BITS:0] foregroundColor,
+    input [CONSOLE_LINES * CONSOLE_COLUMNS * COLOR_NUMBERS_BITS:0] backgroundColor,
     input [15:0] cursorPosition,
     input [3:0]  cursorStates
 );
+
+parameter CONSOLE_LINES = 24;
+parameter CONSOLE_COLUMNS = 80;
+parameter COLOR_NUMBERS_BITS = 4;
+parameter HEIGHT_PER_CHARACTER = 20;
+parameter WIDTH_PER_CHARACTER = 8;
 
 typedef enum logic[1:0]{
     RENDER_INIT, RENDER_INPROCESS, RENDER_WAIT_FOR_FONT, RENDER_DONE
@@ -18,8 +25,23 @@ typedef enum logic[2:0]{
 // read from ROM
 // write to sram
 
-parameter CONSOLE_LINES = 24;
-parameter CONSOLE_COLUMNS = 80;
-parameter COLOR_NUMBERS = 4;
+logic [7:0] charToRender
+logic [COLOR_NUMBERS_BITS - 1:0] currentForegroundColor, currentBackgroundColor;
+logic [HEIGHT_PER_CHARACTER * WIDTH_PER_CHARACTER - 1:0] currentFontColorR, currentFontColorG, currentFontColorB;
+
+FontShapeRenderer #(
+    .COLOR_NUMBERS_BITS(COLOR_NUMBERS_BITS),
+    .HEIGHT_PER_CHARACTER(20)
+    .WIDTH_PER_CHARACTER(8)
+) fontRenderer (
+    .clk(clk),
+    .rst(rst),
+    .char(charToRender),
+    .foregroundColor(currentForegroundColor),
+    .backgroundColor(currentBackgroundColor);
+    .fontColorR(currentFontColorR);
+    .fontColorG(currentFontColorG);
+    .fontColorB(currentFontColorB);
+)
 
 endmodule // TextRenderer
