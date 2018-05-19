c39e42bc78cebf0a86f21f3e32c3d903d563636b
Yuhao Zhou <miskcoo@gmail.com>
2018-05-20 02:32:43 +0800

    Cursor position.

--- src/parser/ActionCursor.sv
+++ src/parser/ActionCursor.sv
@@ -9,6 +9,12 @@ module ActionCursor(
 	output [7:0] o_cursor_x, o_cursor_y
 );
 
+wire [7:0] Pn, Pl, Pc;
+
+assign Pl = (Pn1 == 8'd0) ? 0 : Pn1 - 8'd1;  // line number
+assign Pc = (Pn2 == 8'd0) ? 0 : Pn2 - 8'd1;  // column number
+assign Pn = Pl;   // number 
+
 always @(posedge clk or negedge rst)
 begin
 	if(~rst)
@@ -21,8 +27,30 @@ begin
 		unique case(commandType)
 			CUP:  // Cursor Position
 			begin
-				o_cursor_x <= Pn1 - 8'd1;
-				o_cursor_y <= Pn2 - 8'd1;
+				o_cursor_x <= Pl;
+				o_cursor_y <= Pc;
+			end
+			CUF:  // Cursor Forward 
+				o_cursor_y <= (i_cursor_y + Pn >= `CONSOLE_COLUMNS) ? `CONSOLE_COLUMNS - 1 : i_cursor_y + Pn;
+			CUB:  // Cursor Backward
+				o_cursor_y <= (i_cursor_y < Pn) ? 8'd0 : i_cursor_y - Pn;
+			CUD:  // Cursor Down
+				o_cursor_x <= (i_cursor_x + Pn >= `CONSOLE_LINES) ? `CONSOLE_LINES - 1 : i_cursor_x + Pn;
+			CUU:  // Cursor Up
+				o_cursor_x <= (i_cursor_x < Pn) ? 8'd0 : i_cursor_x - Pn;
+			IND:  // Index
+				o_cursor_x <= (i_cursor_x + 8'd1 >= `CONSOLE_LINES) ? `CONSOLE_LINES - 1 : i_cursor_x + 8'd1;
+			RI:  // Reverse Index
+				o_cursor_x <= (i_cursor_x == 8'd0) ? 8'd0 : i_cursor_x - 8'd1;
+			NEL:  // Next Line
+			begin
+				o_cursor_x <= (i_cursor_x + 8'd1 >= `CONSOLE_LINES) ? `CONSOLE_LINES - 1 : i_cursor_x + 8'd1;
+				o_cursor_y <= 8'd0;
+			end
+			default:
+			begin
+				o_cursor_x <= i_cursor_x;
+				o_cursor_y <= i_cursor_y;
 			end
 		endcase
 	end
