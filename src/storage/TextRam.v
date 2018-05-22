ed915376b086811e4ea523245e15d9effdff3128
Harry Chen <i@harrychen.xyz>
2018-05-22 12:25:01 +0800

    Add another preload textram file
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- src/storage/TextRam.v
+++ src/storage/TextRam.v
@@ -110,7 +110,7 @@ module TextRam (
 		altsyncram_component.clock_enable_output_a = "BYPASS",
 		altsyncram_component.clock_enable_output_b = "BYPASS",
 		altsyncram_component.indata_reg_b = "CLOCK1",
-		altsyncram_component.init_file = "TextRamPoem.mif",
+		altsyncram_component.init_file = "TextRamLogo.mif",
 		altsyncram_component.intended_device_family = "Cyclone IV E",
 		altsyncram_component.lpm_type = "altsyncram",
 		altsyncram_component.numwords_a = 40,
@@ -169,7 +169,7 @@ endmodule
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
 // Retrieval info: PRIVATE: MEMSIZE NUMERIC "102400"
 // Retrieval info: PRIVATE: MEM_IN_BITS NUMERIC "1"
-// Retrieval info: PRIVATE: MIFfilename STRING "TextRamPoem.mif"
+// Retrieval info: PRIVATE: MIFfilename STRING "TextRamLogo.mif"
 // Retrieval info: PRIVATE: OPERATION_MODE NUMERIC "3"
 // Retrieval info: PRIVATE: OUTDATA_ACLR_B NUMERIC "1"
 // Retrieval info: PRIVATE: OUTDATA_REG_B NUMERIC "0"
@@ -203,7 +203,7 @@ endmodule
 // Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_B STRING "BYPASS"
 // Retrieval info: CONSTANT: INDATA_REG_B STRING "CLOCK1"
-// Retrieval info: CONSTANT: INIT_FILE STRING "TextRamPoem.mif"
+// Retrieval info: CONSTANT: INIT_FILE STRING "TextRamLogo.mif"
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone IV E"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
 // Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "40"
