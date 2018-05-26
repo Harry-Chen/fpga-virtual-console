425475cfaac4097c0a964a412fdce61775a6ed4a
Harry Chen <i@harrychen.xyz>
2018-05-27 01:49:15 +0800

    Extract Display Controller and remove unused signals in SignalTap
    Signed-off-by: Harry Chen <i@harrychen.xyz>

---
+++ src/display/DisplayController.sv
@@ -0,0 +1,73 @@
+`include "DataType.svh"
+module DisplayController (
+    input                               clk25M,
+    input                               clk50M,
+    input                               rst,
+    input                               blinkStatus,
+    input   Cursor_t                    cursor,
+    input   TextRamResult_t             textRamResult,
+    output  TextRamRequest_t            textRamRequest,
+    output  SramInterface_t             sramInterface,
+    inout   [`SRAM_DATA_WIDTH - 1:0]    sramData,
+    output  VgaSignal_t                 vga
+    );
+
+    // Sram controller module
+    SramRequest_t vgaRequest, rendererRequest;
+    SramResult_t vgaResult, rendererResult;
+    logic paintDone;
+    SramAddress_t vgaBaseAddress;
+
+    SramController sramController(
+        .clk(clk50M),
+        .rst,
+        .sramInterface,
+        .sramData,
+        .vgaRequest,
+        .vgaResult,
+        .rendererRequest,
+        .rendererResult
+    );
+
+
+    // Font ROM module
+    FontRomData_t fontRomData;
+    FontRomAddress_t fontRomAddress;
+
+    FontRom fontRom(
+        .aclr(rst),
+        .address(fontRomAddress),
+        .clock(clk50M),
+        .q(fontRomData)
+    );
+
+
+    // Renderer module
+    TextRenderer renderer(
+        .clk(clk50M),
+        .rst(rst),
+        .paintDone,
+        .ramRequest(rendererRequest),
+        .ramResult(rendererResult),
+        .vgaBaseAddress,
+        .textRamRequest,
+        .textRamResult,
+        .fontRomAddress,
+        .fontRomData,
+        .cursor,
+        .blinkStatus,
+    );
+
+
+    // VGA module
+    VgaDisplayAdapter display(
+        .clk(clk25M),
+        .rst,
+        .baseAddress(vgaBaseAddress),
+        .ramRequest(vgaRequest),
+        .ramResult(vgaResult),
+        .vga,
+        .paintDone
+    );
+
+endmodule // DisplayController
