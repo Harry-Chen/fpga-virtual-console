52d553e3ab6279c5b3c6688265aa6c7a3bdaaba2
Yuhao Zhou <miskcoo@gmail.com>
2018-06-05 23:23:35 +0800

    Add some demos.

---
+++ demo/set_vt220.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+stty cols 100 rows 50
+export TERM="vt220"
--- demo/video.sh
+++ demo/video.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 stty cols 100 rows 50
-TERM="xterm-256color" mplayer -noautosub -really-quiet -monitorpixelaspect 0.5 -vo caca $1
+TERM="xterm-256color" mplayer -noautosub -really-quiet -monitorpixelaspect 0.666 -vo caca $1
